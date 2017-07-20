b5a4b18b56849c2e5413723458ad23e80391dff7
KorGgenT <curtis.r.merrill@gmail.com>
2017-07-20 11:53:45 -0400

    Add files via upload

--- Thaumaturgic-Machinations_0.1.0/data.lua
+++ Thaumaturgic-Machinations_0.1.0/data.lua
@@ -1,12 +1,13 @@
 if not TM then TM = {} end
-
+require("prototypes.technology")
 require("prototypes.TM-Aspect-Master")
 require("TM-functions")
 require("prototypes.TM-Aspect-Tree-Master")
+require("prototypes.TM-Aspect-Distillation-raw")
+require("prototypes.recipes")
 require("prototypes.TM-Vanilla-Deconstruct")
-
 require("prototypes.entities")
 require("prototypes.item")
-require("prototypes.recipes")
 require("prototypes.categories")
-require("prototypes.TM-Aspect-Distillation-raw")
+require("prototypes.TM-item-aspects")
+require("prototypes.TM-Modules")
